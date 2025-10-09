9b0769151201315af92053adf82d3d2fe24c5669
krassdanek2 <you@example.com>
2025-10-09 05:02:58 +0300

    fix: remove account box and broken images from Rewards page
    - Remove 'Good Morning, Guest' account box section completely
    - Remove all image URLs from reward data (images were not loading)
    - Remove reward-image divs from reward cards
    - Update CSS to remove account-box and reward-image styles
    - Redesign reward cards to be text-only with better styling:
      * White background with rounded corners
      * Box shadow for depth
      * Hover effects with transform and shadow
      * Better spacing and typography
      * Crown cost displayed in highlighted background
    - Simplify reward card layout to show only title and crown cost
    - Maintain responsive design for mobile devices
    - Keep tab navigation and functionality intact

--- src/pages/RewardsPage.jsx
+++ src/pages/RewardsPage.jsx
@@ -12,37 +12,31 @@ const RewardsPage = () => {
       {
         id: 1,
         title: 'Special Promotion!! Tom Yum Chicken with Rice + Beverage 12 oz.',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/7720682-large?time=20250919104851',
         crowns: 0
       },
       {
         id: 2,
         title: 'Imm Sood Chic Set',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/7550350-large?time=20250919104852',
         crowns: 0
       },
       {
         id: 3,
         title: 'Aroi Tid Peek Set',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/7550409-large?time=20250919104856',
         crowns: 0
       },
       {
         id: 4,
         title: 'Single set (Beef or Pork)',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/3298663-large?time=20250919104859',
         crowns: 0
       },
       {
         id: 5,
         title: 'BBQ Bacon Cheese Beef or Pork Set',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/1630842-large?time=20250919104902',
         crowns: 0
       },
       {
         id: 6,
         title: 'Double Cheeseburger Beef or Pork Set',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/1630985-large?time=20250919104905',
         crowns: 0
       }
     ],
@@ -50,19 +44,16 @@ const RewardsPage = () => {
       {
         id: 7,
         title: 'Soft Serve Cone + Small French Fries',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/4376989-large?time=20250919111006',
         crowns: 0
       },
       {
         id: 8,
         title: 'Soft Serve Cone + Small Hash Browns',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/4377196-large?time=20250919111010',
         crowns: 0
       },
       {
         id: 9,
         title: 'Strawberry Sundae + Small French Fries',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/4377272-large?time=20250919112021',
         crowns: 0
       }
     ],
@@ -70,19 +61,16 @@ const RewardsPage = () => {
       {
         id: 10,
         title: 'Buy 1 Get 1 Free! Mushroom Swiss Beef or Pork + French Fries Set',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/6421312-large?time=20250919110951',
         crowns: 0
       },
       {
         id: 11,
         title: 'Hot Deals Fish\'N Crisp Set',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/8217209-large?time=20250919104912',
         crowns: 0
       },
       {
         id: 12,
         title: 'Hot Deals Chic\'N Crisp BBQ Cheese Set',
-        image: 'https://buzzebees.blob.core.windows.net/campaigns/8217221-large?time=20250919110944',
         crowns: 0
       }
     ]
@@ -104,22 +92,6 @@ const RewardsPage = () => {
           <h1 className="rewards-title">{t('rewards')}</h1>
         </div>
 
-        {/* Account Box */}
-        <div className="account-box">
-          <div className="account-content">
-            <div className="unlock-icon">🔓</div>
-            <div className="account-info">
-              <div className="greeting">
-                <span className="greeting-text">{t('goodMorning')}, </span>
-                <span className="guest-name">{t('guest')}</span>
-              </div>
-              <div className="register-text">{t('registerToGetPrivilege')}</div>
-            </div>
-            <div className="info-icon">
-              <a href="/point">ℹ️</a>
-            </div>
-          </div>
-        </div>
 
         {/* Tabs */}
         <div className="rewards-tabs">
@@ -148,10 +120,6 @@ const RewardsPage = () => {
         <div className="rewards-grid">
           {currentRewards.map((reward) => (
             <div key={reward.id} className="reward-card">
-              <div 
-                className="reward-image"
-                style={{ backgroundImage: `url(${reward.image})` }}
-              ></div>
               <div className="reward-info">
                 <div className="reward-title">{reward.title}</div>
                 <div className="reward-crowns">
